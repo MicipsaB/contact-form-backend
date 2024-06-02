1e22195e7d0f5686490c176485941667b47dbc91
Micipsa BEN MAMMAR <micipsa.benmammar@gmail.com>
2024-06-02 21:02:48 +0200

    add sandbox to env

--- index.js
+++ index.js
@@ -24,15 +24,12 @@ app.post("/send-email", async (req, res) => {
     const { firstname, lastname, email, subject, message } = req.body;
 
     // Je demande à mailgun d'envoyer un mail en fonction du body reçu
-    const response = await mg.messages.create(
-      "sandbox9f67e0f664ae45548e2d662404e5f01e.mailgun.org",
-      {
-        from: `${firstname} ${lastname} <${email}>`,
-        to: process.env.EMAIL,
-        subject: subject,
-        text: message,
-      }
-    );
+    const response = await mg.messages.create(process.env.SANDBOX, {
+      from: `${firstname} ${lastname} <${email}>`,
+      to: process.env.EMAIL,
+      subject: subject,
+      text: message,
+    });
 
     //Je réponds au client
     res.json(response);
